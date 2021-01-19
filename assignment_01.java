2dfbfcb20b71d90885a2bf68cd0e65cacb024b3a
hanstoj <stojack.hannah@gmail.com>
2021-01-19 07:04:39 -0800

    all tests pass

--- assignment_01.java
+++ assignment_01.java
@@ -5,10 +5,10 @@
 
 import java.util.Scanner;
 
-public class Assignment01 {
+class Main {
     public static void main(String[] args) {
         // declare and instantiate a Scanner
-        scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
         // declare and initialize variables
         int pizzas;
@@ -30,8 +30,52 @@ public class Assignment01 {
         children = in.nextInt();
 
         // compute required values
+        int totalSlices;
+        totalSlices = pizzas * slices;
+        int adultSlices;
+        adultSlices = adults * 2;
+        int childrenSlices;
+        childrenSlices = totalSlices - adultSlices;
+        int eachChildSlices;
+        eachChildSlices = childrenSlices / children;
+
+        int leftoverSlices;
+        leftoverSlices = totalSlices - (childrenSlices + adultSlices);
 
         // display required outputs
+        // -----------------------------------------------------------------------
+        // I am not sure if this section is required or not so I added incase.
+        // -----------------------------------------------------------------------
 
+        System.out.print("Number of pizzas purchased:  ");
+        System.out.println(pizzas);
+
+        System.out.print("Number of slices per pizza:  ");
+        System.out.println(slices);
+
+        System.out.print("Number of adults:  ");
+        System.out.println(adults);
+
+        System.out.print("Number of children:  ");
+        System.out.println(children);
+
+        // -----------------------------------------------------------------------
+        // Expected Outputs section below.
+        // -----------------------------------------------------------------------
+
+        System.out.print("Total number of slices of pizza:  ");
+        System.out.println(totalSlices);
+
+        System.out.print("Total number of slices given to adults:  ");
+        System.out.println(adultSlices);
+
+        System.out.print("Total number of slices left for children:  ");
+        System.out.println(childrenSlices);
+
+        System.out.print("Total number of slices left for each child:  ");
+        System.out.println(eachChildSlices);
+
+        System.out.print("Leftover slices:  ");
+        System.out.println(leftoverSlices);
     }
 }
