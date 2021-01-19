5e892595d8bb64d1a378086d73766759f94c6905
hanstoj <stojack.hannah@gmail.com>
2021-01-19 05:28:29 -0800

    System input collected

---
+++ assignment_01.java
@@ -0,0 +1,37 @@
+// CSE 110     : CSE 110
+// Assignment  : assignment_01
+// Author      : Hannah Stojack & hstojack
+// Description : <of the file contents>
+
+import java.util.Scanner;
+
+public class Assignment01 {
+    public static void main(String[] args) {
+        // declare and instantiate a Scanner
+        scanner in = new Scanner(System.in);
+
+        // declare and initialize variables
+        int pizzas;
+        int slices;
+        int adults;
+        int children;
+
+        // prompt for and collect inputs
+        System.out.print("How many pizzas? ");
+        pizzas = in.nextInt();
+
+        System.out.print("How many slices? ");
+        slices = in.nextInt();
+
+        System.out.print("How many adults? ");
+        adults = in.nextInt();
+
+        System.out.print("How many children? ");
+        children = in.nextInt();
+
+        // compute required values
+
+        // display required outputs
+
+    }
+}
